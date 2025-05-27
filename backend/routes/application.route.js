f20cdc36b9635b7140c5ea577dcf3fc7416e247e
Radhika Tayal <116450121+Radhikatayal80@users.noreply.github.com>
2025-05-27 08:19:58 +0530

    Update application.route.js

--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -1,15 +1,13 @@
 import express from "express";
+import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
+import { singleUpload } from "../middlewares/mutler.js";
  
 const router = express.Router();
 
-
-router.route("/apply/:id").get(isAuthenticated, applyJob);
-router.route("/get").get( getAppliedJobs);
-router.route("/:id/applicants").get( getApplicants);
-router.route("/status/:id/update").post( updateStatus);
- 
+router.route("/register").post(singleUpload,register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
+router.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
 
 export default router;
-
